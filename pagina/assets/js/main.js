8ba3289002ae08bd67d841dc35d07daeec292e4d
joseeurquidi <josee.urquidi@gmail.com>
2023-08-12 13:10:58 -0600

    proyectos

--- pagina/assets/js/main.js
+++ pagina/assets/js/main.js
@@ -15,6 +15,38 @@ addEventListener("resize", (event) => {
     }
 });
 
+/**
+ * Easy selector helper function
+ */
+const select = (el, all = false) => {
+el = el.trim()
+if (all) {
+    return [...document.querySelectorAll(el)]
+} else {
+    return document.querySelector(el)
+}
+}
+
+/**
+ * Easy event listener function
+ */
+const on = (type, el, listener, all = false) => {
+let selectEl = select(el, all)
+if (selectEl) {
+    if (all) {
+    selectEl.forEach(e => e.addEventListener(type, listener))
+    } else {
+    selectEl.addEventListener(type, listener)
+    }
+}
+}
+
+/**
+ * Easy on scroll event listener 
+ */
+const onscroll = (el, listener) => {
+el.addEventListener('scroll', listener)
+}
 
 /**
  * Back to top button
@@ -31,3 +63,13 @@ const toggleBacktotop = () => {
 window.addEventListener('load', toggleBacktotop)
 onscroll(document, toggleBacktotop)
 }
+
+
+const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+lightbox.option({
+'resizeDuration': 200,
+'wrapAround': true
+})
