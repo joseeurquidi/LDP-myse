1395b831ed81a5a0c8e6923d6c1588f93303f9b5
joseeurquidi <josee.urquidi@gmail.com>
2023-08-11 20:40:26 -0600

    Avance pagina

--- pagina/assets/js/main.js
+++ pagina/assets/js/main.js
@@ -1,16 +1,27 @@
 
-  /**
-   * Back to top button
-   */
-  let backtotop = select('.back-to-top')
-  if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active')
-      } else {
-        backtotop.classList.remove('active')
-      }
+function openMenu(){
+    document.getElementById("container-links").classList.add("active")
+    document.getElementById("hover-interactive-header").classList.add("active")
+}
+
+function closeMenu(){
+    document.getElementById("container-links").classList.remove("active")
+    document.getElementById("hover-interactive-header").classList.remove("active")
+}
+
+
+/**
+ * Back to top button
+ */
+let backtotop = select('.back-to-top')
+if (backtotop) {
+const toggleBacktotop = () => {
+    if (window.scrollY > 100) {
+    backtotop.classList.add('active')
+    } else {
+    backtotop.classList.remove('active')
     }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
-  }
+}
+window.addEventListener('load', toggleBacktotop)
+onscroll(document, toggleBacktotop)
+}
