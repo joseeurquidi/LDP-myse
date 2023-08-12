e9b054dc207d75fb49c2a095af65effc64069c0d
joseeurquidi <josee.urquidi@gmail.com>
2023-08-12 13:44:43 -0600

    clientes

--- pagina/assets/js/main.js
+++ pagina/assets/js/main.js
@@ -73,3 +73,20 @@ lightbox.option({
 'resizeDuration': 200,
 'wrapAround': true
 })
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
